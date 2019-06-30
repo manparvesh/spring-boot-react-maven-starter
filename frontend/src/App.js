c18c8c96d5bbf2b649b0def752286984147bfc09
Man Parvesh Singh Randhawa <manparveshsinghrandhawa@gmail.com>
2019-06-30 16:36:41 +0800

    add some react code to confirm integration with the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:8080/api/ping')
+      .then(results => {
+        return results.json();
+      })
+      .then(data => {
+        this.setState({data: data});
+      })
+    ;
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h2>Welcome to Spring Boot React Starter!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          This is the data we ping the spring boot backend: <code>{this.state.data.ping}</code>
         </p>
       </div>
     );
